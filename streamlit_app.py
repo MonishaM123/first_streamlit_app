300fbdbea1a9ea709821b033439aaea588648edc
MM <121984927+MonishaM123@users.noreply.github.com>
2023-01-04 17:28:45 -0600

    Update streamlit_app.py
    Added Texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit
 streamlit.title('Healthy Diner')
 
 streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Hard Boiled Free-Range Egg')
